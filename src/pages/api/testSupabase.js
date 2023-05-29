7c66e65300f6d198283d63e04e20f85811f40363
John Brady <johnbradydeveloper@gmail.com>
2023-05-28 21:10:53 -0700

    adjusting table tests

--- src/pages/api/testSupabase.js
+++ src/pages/api/testSupabase.js
@@ -1,7 +1,7 @@
 import { supabase } from '../../utils/supabaseClient'
 
 export default async function handler(req, res) {
-  const { data, error } = await supabase.from('test_table').select('*')
+  const { data, error } = await supabase.from('test_table').select('name')
 
   if (error) {
     return res.status(500).json({ error: error.message })
